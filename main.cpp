331cf8209e8776d0cc9af952ccf175f3f5bd2ef9
Basanta Khakurel <basanta.khakurel@selu.edu>
2022-07-19 16:12:17 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main (int argc, char* argv[]) {
+std::cout << "BabyBayes v1.0\n";
+std::cout << "Basanta Khakurel\n";
+std::cout << "Southeastern Louisiana University\n";
+std::cout << "\n\n";
+return 0;
+}
+
